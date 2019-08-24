c6eced1eaf83cca4ed89a67ac5868f816d21995d
Tuan Phan <phanquoctuan00@gmail.com>
2019-08-24 19:48:57 +0700

    add
    add

---
+++ Project1/Project1/bai2.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+	int a, b, c, s;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	s = a + b + c;
+
+	cout << s;
+	return 0;
+}
