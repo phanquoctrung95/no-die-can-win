12c76f20208db0d665d4124418f7cb1f2103ef5a
Tuan Phan <phanquoctuan00@gmail.com>
2019-11-29 23:57:53 +0700

    format

--- ThaoTacVoiFile/ThaoTacVoiFile/ThaoTacVoiFile/MSSV.cpp
+++ ThaoTacVoiFile/ThaoTacVoiFile/ThaoTacVoiFile/MSSV.cpp
@@ -7,8 +7,8 @@ void XuatFileChiSoLe(char* tenFile, int* arr, int n)
 	//mo file de ghi
 	FILE* fo = fopen(tenFile, "wt"); // wt = write (ghi) + text (dang van ban)
 	//ghi du lieu ra file
-	for (int i = 1; i < n; i=i+2)
-	fprintf(fo, "%d ", arr[i]);
+	for (int i = 1; i < n; i = i + 2)
+		fprintf(fo, "%d ", arr[i]);
 	// dong file
 	fclose(fo);
 }
@@ -40,25 +40,25 @@ int main()
 		cout << "\n1.Xuat gia tri o vi tri le trong mang ra mang hinh va File ";
 		cout << "\n2.Xuat tong ra mang hinh va File ";
 		cout << "\nNhap cong viec thuc hien : \n";
-		cin >>chon;
-		switch(chon){
+		cin >> chon;
+		switch (chon){
 		case 1:for (int i = 1; i < n; i = i + 2){
-				   cout << arr[i]<<" ";
+				   cout << arr[i] << " ";
 		}
 			   XuatFileChiSoLe("out_MSSV.txt", arr, n);
-				break;
+			   break;
 		case 2:for (int i = 0; i < n; i++){
 				   s = s + arr[i];
 		}
 			   cout << s;
 			   XuatFileTong("out_MSSV.txt", arr, n);
-				break;
-			default:
-				break;
+			   break;
+		default:
+			break;
 		}
 		system("pause");
 		system("cls");
 		goto tt;
-	}while(chon!=0);
+	} while (chon != 0);
 tt:;
 }
