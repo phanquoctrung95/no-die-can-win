293e6d2ebc5a8259f0543addc65d1c74ae3db4b0
Tuan Phan <phanquoctuan00@gmail.com>
2019-11-29 23:53:32 +0700

    Sửa sai

--- ThaoTacVoiFile/ThaoTacVoiFile/ThaoTacVoiFile/MSSV.cpp
+++ ThaoTacVoiFile/ThaoTacVoiFile/ThaoTacVoiFile/MSSV.cpp
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <stdio.h>
 #include <iostream>
-void XuatFile(char* tenFile, int* arr, int n)
+using namespace std;
+void XuatFileChiSoLe(char* tenFile, int* arr, int n)
 {
-	int s = 0;
 	//mo file de ghi
 	FILE* fo = fopen(tenFile, "wt"); // wt = write (ghi) + text (dang van ban)
 	//ghi du lieu ra file
-	for (int i = 0; i < n; i++)
-		s = s + i;
-	fprintf(fo, "%d ", s);
+	for (int i = 1; i < n; i=i+2)
+	fprintf(fo, "%d ", arr[i]);
 	// dong file
 	fclose(fo);
 }
-void main()
+void XuatFileTong(char* tenFile, int* arr, int n){
+	int s = 0;
+	FILE* fo = fopen(tenFile, "wt");
+	for (int i = 0; i < n; i++){
+		s = s + arr[i];
+	}
+	fprintf(fo, "%d", s);
+	fclose(fo);
+}
+int main()
 {
 	// mo file de doc
 	FILE* fi = fopen("In_MSSV.txt", "rt");
@@ -26,12 +34,31 @@ void main()
 		fscanf(fi, "%d", &arr[i]);
 	//dong file
 	fclose(fi);
-	// in ra man hinh de kiem tra
-	for (int i = 0; i < n; i++)
-		s = s + i;
-	printf("%d", s);
-	system("pause");
-	printf("\n");
-	// xuat ra file
-	XuatFile("out_MSSV.txt", arr, n);
+	int chon;
+	do{
+		cout << "Moi ban chon : ";
+		cout << "\n1.Xuat gia tri o vi tri le trong mang ra mang hinh va File ";
+		cout << "\n2.Xuat tong ra mang hinh va File ";
+		cout << "\nNhap cong viec thuc hien : \n";
+		cin >>chon;
+		switch(chon){
+		case 1:for (int i = 1; i < n; i = i + 2){
+				   cout << arr[i]<<" ";
+		}
+			   XuatFileChiSoLe("out_MSSV.txt", arr, n);
+				break;
+		case 2:for (int i = 0; i < n; i++){
+				   s = s + arr[i];
+		}
+			   cout << s;
+			   XuatFileTong("out_MSSV.txt", arr, n);
+				break;
+			default:
+				break;
+		}
+		system("pause");
+		system("cls");
+		goto tt;
+	}while(chon!=0);
+tt:;
 }
