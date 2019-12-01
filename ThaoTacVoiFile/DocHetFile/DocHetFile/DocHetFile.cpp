039e1a2dcd3fb97738742eeae4982652b5d9e6e8
Tuan Phan <phanquoctuan00@gmail.com>
2019-12-01 15:21:34 +0700

    update all
    có giải thích

---
+++ ThaoTacVoiFile/DocHetFile/DocHetFile/DocHetFile.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <iostream>
+void DocHetFile1(char* tenFile)
+{
+	FILE* fi = fopen(tenFile, "rt");
+	float temp;
+	while (!feof(fi))
+	{
+		fscanf(fi, "%f", &temp);
+		printf("%0.1f ", temp); 
+	}
+	printf("\n");
+	fclose(fi);
+}
+void DocHetFile2(char* tenFile)
+{
+	FILE* fi = fopen(tenFile, "rt");
+	float temp;
+	while (!feof(fi))
+	{
+		if (fscanf(fi, "%f", &temp)>0)
+			printf("%0.1f ", temp);
+		else
+			break;
+	}
+	system("pause");
+	printf("\n");
+	fclose(fi);
+}
+void main()
+{
+	printf("Doc khi khong biet so luong phan tu, doc den het file thi dung:\n");
+	DocHetFile2("input2.txt");
+	DocHetFile2("input3.txt");
+	system("pause");
+}
---
+++ ThaoTacVoiMang/readme.txt
@@ -0,0 +1,4 @@
+exe: thư mục chứ file *.exe dùng để run chương trình.
+[Ten Solution]: chính là project tạo trong visuastudio, chọn CLEAN SOLUTION trước khi commit và push lên git
+resource: chứa đề bài
+
